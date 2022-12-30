456240e665d7a8cbe85f1fe507830e012692579e
Gaby-Gom <la_gaby_gomez@hotmail.com>
2022-12-30 16:49:07 -0300

    Se agrega el proyecto

---
+++ js/products.js
@@ -0,0 +1,35 @@
+const productos=[
+    {
+        id:1,
+        nombre:"Harina",
+        precio:50,
+        img:src="./img/harina.jpg",
+        cantidad:1,
+
+    },
+    {
+        id:3,
+        nombre:"Cerveza",
+        precio:150,
+        img:src="./img/cerveza.jpg",
+        cantidad:1,
+
+    },
+
+    {
+        id:4,
+        nombre:"Leche",
+        precio:200,
+        img:src="./img/leche.jpg",
+        cantidad:1,
+
+    },
+
+    {
+        id:5,
+        nombre:"Galletas",
+        precio:100,
+        img:src="./img/galletas.jpg",
+        cantidad:1,
+    },
+];
